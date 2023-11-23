95eb0923671cc726602297b485d938d05a969772
kajok88 <skyborgg88@gmail.com>
2023-11-23 15:03:50 +0200

    updated for 3.1

--- index.js
+++ index.js
@@ -3,48 +3,32 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [  {    
-    id: 1,    
-    content: "HTML is easy, nice",    
-    important: true  
-},  
-{    
-    id: 2,    
-    content: "Browser can execute only JavaScript",    
-    important: false  
-},  
-{    
-    id: 3,    
-    content: "GET and POST are the most important methods of HTTP protocol",    
-    important: true  
-}]
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/notes', (req, res) => {
-  res.json(notes)
+app.get('/persons', (req, res) => {
+  res.json(persons)
 })
 
-app.get('/notes/:id', (request, response) => {
+app.get('/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {    
-    response.json(note)  
+  const person = persons.find(person => person.id === id)
+  if (person) {    
+    response.json(person)  
   } else {    
     response.status(404).end()  
   }
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
 
-app.post('/notes', (request, response) => {
+app.post('/persons', (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -53,20 +37,20 @@ app.post('/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const person = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
-app.delete('/notes/:id', (request, response) => {
+app.delete('/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
@@ -76,3 +60,30 @@ const PORT = 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+
+
+
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: 1
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: 2
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
+    }
+  ]
